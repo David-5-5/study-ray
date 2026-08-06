6a503cc83e0883f77a848af49547ccd6b20b3b12
David-5-5 <13910767907@139.com>
2026-08-06 15:46:03 +0800

    add grad backward, no‑grad context, detach and gradient zero demo

--- study-ray/torch/basic/autograd.py
+++ study-ray/torch/basic/autograd.py
@@ -56,3 +56,44 @@ print(a.grad_fn)
 out.backward()
 print(a.grad)
 plt.plot(a.detach(), a.grad.detach())
+plt.show()
+
+
+# y = w * x + b
+x = torch.ones(5)
+y = torch.zeros(3)
+w = torch.randn(5, 3, requires_grad=True)
+b = torch.randn(3, requires_grad=True)
+z = torch.matmul(x, w) + b
+loss = torch.nn.functional.binary_cross_entropy_with_logits(z, y)
+
+print(f"Gradient function for z = {z.grad_fn}")
+print(f"Gradient function for loss = {loss.grad_fn}")
+
+loss.backward()
+print(w.grad)
+print(b.grad)
+
+z = torch.matmul(x, w) + b
+print(z.requires_grad)
+
+with torch.no_grad():
+    z = torch.matmul(x, w) + b
+print(z.requires_grad)
+
+z = torch.matmul(x, w) + b
+z_det = z.detach()
+print(z_det.requires_grad)
+
+inp = torch.eye(4, 5, requires_grad=True)
+out = (inp + 1).pow(2).t()
+
+out.backward(torch.ones_like(out), retain_graph=True)
+print(f"Fisrt call \n {inp.grad}")
+
+out.backward(torch.ones_like(out), retain_graph=True)
+print(f"Second call \n {inp.grad}")
+
+inp.grad.zero_()
+out.backward(torch.ones_like(out), retain_graph=True)
+print(f"\nCall after zeroing gradients\n {inp.grad}")
