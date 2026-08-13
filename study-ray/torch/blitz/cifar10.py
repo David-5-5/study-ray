1823f8b73d7e27fa2615f3f04aab34f710ba6c65
David-5-5 <13910767907@139.com>
2026-08-13 18:56:46 +0800

    feat: add CNN end-to-end training and evaluation pipeline for image classification

--- study-ray/torch/blitz/cifar10.py
+++ study-ray/torch/blitz/cifar10.py
@@ -1,3 +1,5 @@
+from email.mime import image
+import os
 import torch
 import torchvision
 from torchvision.transforms import v2
@@ -37,3 +39,110 @@ images, labels = next(dataiter)
 imshow(torchvision.utils.make_grid(images))
 print(' '.join(f'{classes[labels[j]]:5s}' for j in range(batch_size)))
 
+
+# copy and modified from ./neural_network.py
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        # input image channel, 6 output channels, 5*5 square convolution kernel
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2) 
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        # an affine operation: y = Wx + b
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+    
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+net = Net()
+
+
+import torch.optim as optim
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2): # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print(f'[{epoch + 1}, {i+1:5d}] loss " {running_loss/2000:.3f}')
+            running_loss = 0.0
+print('Finishing Training')
+
+PATH = os.path.expanduser("~/.data/cifar_net.pt")
+torch.save(net.state_dict(), PATH)
+
+
+dataiter = iter(testloader)
+images, labels = next(dataiter)
+
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruht: ', ' '.join(f'{classes[labels[j]]:5s}' for j in range(4)))
+
+
+net = Net()
+net.load_state_dict(torch.load(PATH, weights_only=True))
+
+outputs = net(images)
+
+_, predicted =  torch.max(outputs, 1)
+print('Predicted: ', ' '.join(f'{classes[predicted[j]]:5s}' for j in range(4)))
+
+correct = 0
+total = 0
+
+# since we're not training ,we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images" {100 * correct // total}%')
+
+correct_pred = {classname: 0 for classname in classes}
+total_pred = {classname: 0 for classname in classes}
+
+# again no gradients needed
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predictions = torch.max(outputs, 1)
+        for label, prediction in zip(labels, predictions):
+            if label == prediction:
+                correct_pred[classes[label]] += 1
+            total_pred[classes[label]] += 1
+
+# print accurary for each class
+for classname, correct_count in correct_pred.items():
+    accuracy = 100 * float(correct_count)/ total_pred[classname]
+    print(f'Accuracy for class" {classname:5s} is {accuracy:.1f}%')
